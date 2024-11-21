eb3b67e3f619c80616093221e4941933cd9db9ca
dxbvo <diego.belusky@icloud.com>
2024-11-21 18:03:24 +0100

    new design and content

---
+++ README.md
@@ -0,0 +1,4 @@
+# Portfolio Webpage
+
+This repository contains the code for my personal portfolio webpage which an be found [here](https://dxbvo.github.io/).
+
--- script.js
+++ script.js
@@ -7,4 +7,18 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
     });
   });
+
+  window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+})
+
+
+
   
